94b55cb35c77134555143c7a0c9e55324d895073
Marco <109109273+marcocado@users.noreply.github.com>
2022-12-22 09:18:10 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,81 +1,98 @@
 #include<stdio.h>
 #include<math.h>
 
-struct mValues{
-    double m1;
-    double m2;
-    double m3;
-    double m4;
-};
-
-struct kValues{
-    double k1;
-    double k2;
-    double k3;
-    double k4;
-};
-
-struct funcValues{
-    double xn;
-    double yn;
-    double ydn;
-};
-
 double function(double x, double y, double yd){
     /*
-    Here you can assert the equation for y'' = f(x, y, y'), you can also use math.h functions
+    function:   y'' = f(x, y, y')
     */
-   double f = x + yd - y;
-   return f;
+    return -2*yd + 3*y + x;
 }
 
-double approximation(struct funcValues *currP, struct kValues *k, struct mValues *m, double h){
+
+void RungeKutta4(double *xArray, double *yArray, double *ydArray, double *yddArray, double h, int currStep){
     /*
-    First of all, the helper variables k1, k2, k3, k4 and m1, m2, m3, m4 are calculated. For better 
-    readability and memory management the helper variables and function values are passed in with pointers
-    to the structures.
-    Afterwards y and y' of the current step are calculated.
+    Classical Runge-Kutta 4th order method for 2nd order ordinary differential equations
     */
-    k->k1 = h*currP->ydn;
-    m->m1 = h*function(currP->xn, currP->yn, currP->ydn);
-    k->k2 = h*(currP->ydn + m->m1/2);
-    m->m2 = h*function(currP->xn + h/2, currP->yn + k->k1/2, currP->ydn + m->m1/2);
-    k->k3 = h*(currP->ydn + m->m2/2);
-    m->m3 = h*function(currP->xn + h/2, currP->yn + k->k2/2, currP->ydn + m->m2/2);
-    k->k4 = h*(currP->ydn + m->m3);
-    m->m4 = h*function(currP->xn + h, currP->yn + k->k3, currP->ydn + m->m3);
+    double k1, k2, k3, k4;
+    double m1, m2, m3, m4;
+    int beforeStep = currStep - 1;
 
-    currP->yn = currP->yn + 0.166666*(k->k1 + 2*k->k2 + 2*k->k3 + k->k4);
-    currP->ydn = currP->yn + 0.166666*(m->m1 + 2*m->m2 + 2*m->m3 + m->m4);
+    k1 = h*ydArray[beforeStep];
+    m1 = h*function(xArray[beforeStep], yArray[beforeStep], ydArray[beforeStep]);
+    k2 = h*(ydArray[beforeStep] + m1/2);
+    m2 = h*function(xArray[beforeStep]+h/2, yArray[beforeStep]+k1/2, ydArray[beforeStep]+m1/2);
+    k3 = h*(ydArray[beforeStep] + m2/2);
+    m3 = h*function(xArray[beforeStep]+h/2, yArray[beforeStep]+k2/2, ydArray[beforeStep]+m2/2);
+    k4 = h*(ydArray[beforeStep] + m3);
+    m4 = h*function(xArray[beforeStep]+h, yArray[beforeStep]+k3, ydArray[beforeStep]+m3);
+
+    xArray[currStep] = xArray[beforeStep] + h;
+    yArray[currStep] = yArray[beforeStep] + (1.0/6.0)*(k1 + 2*k2 + 2*k3 + k4);
+    ydArray[currStep] = ydArray[beforeStep] + (1.0/6.0)*(m1 + 2*m2 + 2*m3 + m4);
+    yddArray[currStep] = function(xArray[beforeStep], yArray[beforeStep], ydArray[beforeStep]);
 }
 
-void main(){
-    // Initialize the variables & structs
-    uint i;
-    uint n = 40;
-    double a, b, h;
-    struct funcValues startValues = {1, 2, 1};
 
-    // Calculate the step increment
-    a = startValues.xn;
-    b = 12;
-    h = (b - a)/n;
-    printf("The step increment is: %.2f\n", h);
+void printCalculation(double *xArray, double *yArray, double *ydArray, double *yddArray, int currStep){
+    printf("----------------------------\n");
+    printf("x:      %.3f\n", xArray[currStep]);
+    if(yArray[currStep]>10000){
+        printf("y:      %.4e\n", yArray[currStep]);
+    }
+    else{
+        printf("y:      %.3f\n", yArray[currStep]);
+    }
+    if(ydArray[currStep]>10000){
+        printf("yd:     %.4e\n", ydArray[currStep]);
+    }
+    else{
+        printf("yd:     %.3f\n", ydArray[currStep]);
+    }
+    if(yddArray[currStep]>10000){
+        printf("ydd:    %.4e\n", yddArray[currStep]);
+    }
+    else{
+        printf("ydd:    %.3f\n", yddArray[currStep]);
+    }
+    printf("----------------------------\n\n");
+}
 
-    // Initialize the struct and struct pointers
-    struct kValues k;
-    struct kValues *kPointer = &k;
-    struct mValues m;
-    struct mValues *mPointer = &m;
-    struct funcValues f = startValues;
-    struct funcValues *fPointer = &f;
 
-    // Iterate through all step increments and calculate y and y'
-    for(i=0; i<n; i++){
-        approximation(fPointer, kPointer, mPointer, h);
-        fPointer->xn = fPointer->xn + h;
-        printf("\nFor the current step %.2f\n", fPointer->xn);
-        printf("y:  %.3f\n", fPointer->yn);
-        printf("y': %.3f\n", fPointer->ydn);
+void main(void){
+    unsigned int i;
+    double a = 0, b = 1;
+    unsigned int n = 2000;
+    double h;
+    unsigned int sizeArray = n+1;
+
+    h = (b - a)/ (double) n;
+
+    double y0 = 0;
+    double yd0 = 4;
+
+    double xArray[sizeArray];
+    double (*ptrxArray)[sizeArray];
+    ptrxArray = &xArray;
+
+    double yArray[sizeArray];
+    double (*ptryArray)[sizeArray];
+    ptryArray = &yArray;
+
+    double ydArray[sizeArray];
+    double (*ptrydArray)[sizeArray];
+    ptrydArray = &ydArray;
+
+    double yddArray[sizeArray];
+    double (*ptryddArray)[sizeArray];
+    ptryddArray = &yddArray;
+    
+    (*ptrxArray)[0] = a;
+    (*ptryArray)[0] = y0;
+    (*ptrydArray)[0] = yd0;
+    (*ptryddArray)[0] = function(a, y0, yd0);
+
+    for(i=1;i<sizeArray;i++){
+        RungeKutta4(*ptrxArray, *ptryArray, *ptrydArray, *ptryddArray, h, i);
+        printCalculation(*ptrxArray, *ptryArray, *ptrydArray, *ptryddArray, i);
     }
 }
