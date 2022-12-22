f193123ef684e28d0cd5601759e91dca8ef4f655
Marco <109109273+marcocado@users.noreply.github.com>
2022-12-22 09:39:09 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 
+
 double function(double x, double y, double yd){
     /*
     function:   y'' = f(x, y, y')
@@ -8,8 +9,7 @@ double function(double x, double y, double yd){
     return -2*yd + 3*y + x;
 }
 
-
-void RungeKutta4(double *xArray, double *yArray, double *ydArray, double *yddArray, double h, int currStep){
+void RungeKutta4(double (*xArray)[], double (*yArray)[], double (*ydArray)[], double (*yddArray)[], double h, int currStep){
     /*
     Classical Runge-Kutta 4th order method for 2nd order ordinary differential equations
     */
@@ -17,23 +17,28 @@ void RungeKutta4(double *xArray, double *yArray, double *ydArray, double *yddArr
     double m1, m2, m3, m4;
     int beforeStep = currStep - 1;
 
-    k1 = h*ydArray[beforeStep];
-    m1 = h*function(xArray[beforeStep], yArray[beforeStep], ydArray[beforeStep]);
-    k2 = h*(ydArray[beforeStep] + m1/2);
-    m2 = h*function(xArray[beforeStep]+h/2, yArray[beforeStep]+k1/2, ydArray[beforeStep]+m1/2);
-    k3 = h*(ydArray[beforeStep] + m2/2);
-    m3 = h*function(xArray[beforeStep]+h/2, yArray[beforeStep]+k2/2, ydArray[beforeStep]+m2/2);
-    k4 = h*(ydArray[beforeStep] + m3);
-    m4 = h*function(xArray[beforeStep]+h, yArray[beforeStep]+k3, ydArray[beforeStep]+m3);
+    k1 = h*(*ydArray)[beforeStep];
+    m1 = h*function((*xArray)[beforeStep], (*yArray)[beforeStep], (*ydArray)[beforeStep]);
 
-    xArray[currStep] = xArray[beforeStep] + h;
-    yArray[currStep] = yArray[beforeStep] + (1.0/6.0)*(k1 + 2*k2 + 2*k3 + k4);
-    ydArray[currStep] = ydArray[beforeStep] + (1.0/6.0)*(m1 + 2*m2 + 2*m3 + m4);
-    yddArray[currStep] = function(xArray[beforeStep], yArray[beforeStep], ydArray[beforeStep]);
+    k2 = h*((*ydArray)[beforeStep] + m1/2);
+    m2 = h*function((*xArray)[beforeStep]+h/2, (*yArray)[beforeStep]+k1/2, (*ydArray)[beforeStep]+m1/2);
+
+    k3 = h*((*ydArray)[beforeStep] + m2/2);
+    m3 = h*function((*xArray)[beforeStep]+h/2, (*yArray)[beforeStep]+k2/2, (*ydArray)[beforeStep]+m2/2);
+
+    k4 = h*((*ydArray)[beforeStep] + m3);
+    m4 = h*function((*xArray)[beforeStep]+h, (*yArray)[beforeStep]+k3, (*ydArray)[beforeStep]+m3);
+
+    (*xArray)[currStep] = (*xArray)[beforeStep] + h;
+    (*yArray)[currStep] = (*yArray)[beforeStep] + (1.0/6.0)*(k1 + 2*k2 + 2*k3 + k4);
+    (*ydArray)[currStep] = (*ydArray)[beforeStep] + (1.0/6.0)*(m1 + 2*m2 + 2*m3 + m4);
+    (*yddArray)[currStep] = function((*xArray)[beforeStep], (*yArray)[beforeStep], (*ydArray)[beforeStep]);
 }
 
-
 void printCalculation(double *xArray, double *yArray, double *ydArray, double *yddArray, int currStep){
+    /*
+    Print the output for each calculation step
+    */
     printf("----------------------------\n");
     printf("x:      %.3f\n", xArray[currStep]);
     if(yArray[currStep]>10000){
@@ -60,13 +65,14 @@ void printCalculation(double *xArray, double *yArray, double *ydArray, double *y
 
 void main(void){
     unsigned int i;
-    double a = 0, b = 1;
-    unsigned int n = 2000;
+    double a = 0, b = 1;    // Start and end values of the range of the calculation
+    unsigned int n = 2000;  // Number of calculation steps
     double h;
     unsigned int sizeArray = n+1;
 
     h = (b - a)/ (double) n;
 
+    // Boundaries conditions for the second order differential equation
     double y0 = 0;
     double yd0 = 4;
 
@@ -86,6 +92,7 @@ void main(void){
     double (*ptryddArray)[sizeArray];
     ptryddArray = &yddArray;
     
+    // Assign the start conditions
     (*ptrxArray)[0] = a;
     (*ptryArray)[0] = y0;
     (*ptrydArray)[0] = yd0;
