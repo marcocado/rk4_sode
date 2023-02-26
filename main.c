22d88498dec658ea55a06d1a01e6d7a3cad898ff
Marco <109109273+marcocado@users.noreply.github.com>
2023-02-26 10:17:40 +0100

    Updated main.c
    Changed some variable names to more cohesive ones

--- main.c
+++ main.c
@@ -11,67 +11,67 @@ double function(double x, double y, double yd){
     return -2.0*yd + 3.0*y + 2.0*x;
 }
 
-void stepCalculation(double (*xArray)[], double (*yArray)[], double (*ydArray)[], double (*yddArray)[], double h, int currStep){
+void calculate_step(double (*xArray)[], double (*yArray)[], double (*ydArray)[], double (*yddArray)[], double h, int current_step){
     /*
     Calculation of the classical Runge-Kutta 4th order method for 2nd order ordinary differential equations for one step
     */
     double k1, k2, k3, k4;
     double m1, m2, m3, m4;
-    int beforeStep = currStep - 1;
+    int before_step = current_step - 1;
 
     // Calculation of the temporary helper variables
-    k1 = h*(*ydArray)[beforeStep];
-    m1 = h*function((*xArray)[beforeStep], (*yArray)[beforeStep], (*ydArray)[beforeStep]);
-    k2 = h*((*ydArray)[beforeStep] + m1/2.0);
-    m2 = h*function((*xArray)[beforeStep]+h/2.0, (*yArray)[beforeStep]+k1/2.0, (*ydArray)[beforeStep]+m1/2.0);
-    k3 = h*((*ydArray)[beforeStep] + m2/2.0);
-    m3 = h*function((*xArray)[beforeStep]+h/2.0, (*yArray)[beforeStep]+k2/2.0, (*ydArray)[beforeStep]+m2/2.0);
-    k4 = h*((*ydArray)[beforeStep] + m3);
-    m4 = h*function((*xArray)[beforeStep]+h, (*yArray)[beforeStep]+k3, (*ydArray)[beforeStep]+m3);
+    k1 = h*(*ydArray)[before_step];
+    m1 = h*function((*xArray)[before_step], (*yArray)[before_step], (*ydArray)[before_step]);
+    k2 = h*((*ydArray)[before_step] + m1/2.0);
+    m2 = h*function((*xArray)[before_step]+h/2.0, (*yArray)[before_step]+k1/2.0, (*ydArray)[before_step]+m1/2.0);
+    k3 = h*((*ydArray)[before_step] + m2/2.0);
+    m3 = h*function((*xArray)[before_step]+h/2.0, (*yArray)[before_step]+k2/2.0, (*ydArray)[before_step]+m2/2.0);
+    k4 = h*((*ydArray)[before_step] + m3);
+    m4 = h*function((*xArray)[before_step]+h, (*yArray)[before_step]+k3, (*ydArray)[before_step]+m3);
 
     // Calculation and assignment of the variables with pointers
-    (*xArray)[currStep] = (*xArray)[beforeStep] + h;
-    (*yArray)[currStep] = (*yArray)[beforeStep] + (1.0/6.0)*(k1 + 2.0*k2 + 2.0*k3 + k4);
-    (*ydArray)[currStep] = (*ydArray)[beforeStep] + (1.0/6.0)*(m1 + 2.0*m2 + 2.0*m3 + m4);
-    (*yddArray)[currStep] = function((*xArray)[beforeStep], (*yArray)[beforeStep], (*ydArray)[beforeStep]);
+    (*xArray)[current_step] = (*xArray)[before_step] + h;
+    (*yArray)[current_step] = (*yArray)[before_step] + (1.0/6.0)*(k1 + 2.0*k2 + 2.0*k3 + k4);
+    (*ydArray)[current_step] = (*ydArray)[before_step] + (1.0/6.0)*(m1 + 2.0*m2 + 2.0*m3 + m4);
+    (*yddArray)[current_step] = function((*xArray)[before_step], (*yArray)[before_step], (*ydArray)[before_step]);
 }
 
-void printCalculation(double (*xArray)[], double (*yArray)[], double (*ydArray)[], double (*yddArray)[], int currStep){
+void print_step(double (*xArray)[], double (*yArray)[], double (*ydArray)[], double (*yddArray)[], int current_step){
     /*
     Print the output for each calculation step
     */
     printf("----------------------------\n");
-    printf("x:      %.3f\n", (*xArray)[currStep]);
-    if((*yArray)[currStep]>10000){
-        printf("y:      %.4e\n", (*yArray)[currStep]);
+    printf("x:      %.3f\n", (*xArray)[current_step]);
+    if((*yArray)[current_step]>10000){
+        printf("y:      %.4e\n", (*yArray)[current_step]);
     }
     else{
-        printf("y:      %.3f\n", (*yArray)[currStep]);
+        printf("y:      %.3f\n", (*yArray)[current_step]);
     }
-    if((*ydArray)[currStep]>10000){
-        printf("yd:     %.4e\n", (*ydArray)[currStep]);
-    }
-    else{
-        printf("yd:     %.3f\n", (*ydArray)[currStep]);
-    }
-    if((*yddArray)[currStep]>10000){
-        printf("ydd:    %.4e\n", (*yddArray)[currStep]);
+    if((*ydArray)[current_step]>10000){
+        printf("yd:     %.4e\n", (*ydArray)[current_step]);
     }
     else{
-        printf("ydd:    %.3f\n", (*yddArray)[currStep]);
+        printf("yd:     %.3f\n", (*ydArray)[current_step]);
+    }
+    if((*yddArray)[current_step]>10000){
+        printf("ydd:    %.4e\n", (*yddArray)[current_step]);
+    }
+    else{
+        printf("ydd:    %.3f\n", (*yddArray)[current_step]);
     }
     printf("----------------------------\n\n");
 }
 
-void RungeKutta4(int sizeArray, double (*xArray)[], double (*yArray)[], double (*ydArray)[], double (*yddArray)[], double h){
+void runga_kutta_4(int sizeArray, double (*xArray)[], double (*yArray)[], double (*ydArray)[], double (*yddArray)[], double h){
     /*
     Iterate through all points and calculate the x, y, y' and y'' values for the step
     */
     int i;
-    printCalculation(*xArray, *yArray, *ydArray, *yddArray, 0);
+    print_step(*xArray, *yArray, *ydArray, *yddArray, 0);
     for(i=1;i<sizeArray;i++){
-        stepCalculation(*xArray, *yArray, *ydArray, *yddArray, h, i);
-        printCalculation(*xArray, *yArray, *ydArray, *yddArray, i);
+        calculate_step(*xArray, *yArray, *ydArray, *yddArray, h, i);
+        print_step(*xArray, *yArray, *ydArray, *yddArray, i);
     }
 }
 
@@ -135,7 +135,7 @@ int main(void){
         (*ptryddArray)[0] = function(a, y0, yd0);
 
         // Start the iterative calculation process
-        RungeKutta4(sizeArray, *ptrxArray, *ptryArray, *ptrydArray, *ptryddArray, h);
+        runga_kutta_4(sizeArray, *ptrxArray, *ptryArray, *ptrydArray, *ptryddArray, h);
     }
     else{
         printf("Due to lacking memory allocation, the numerical process can't be started\n");
